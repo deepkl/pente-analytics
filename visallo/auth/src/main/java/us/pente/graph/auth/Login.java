b0a6b3f11cdc1cf67ff923aa5fa018b8834d034b
Steven Farley <srfarley@gmail.com>
2016-08-28 08:11:23 -0400

    Add command-line application to fetch game archives from pente.org

--- visallo/auth/src/main/java/us/pente/graph/auth/Login.java
+++ visallo/auth/src/main/java/us/pente/graph/auth/Login.java
@@ -1,6 +1,5 @@
 package us.pente.graph.auth;
 
-import com.google.common.base.Throwables;
 import com.google.inject.Inject;
 import com.v5analytics.webster.ParameterizedHandler;
 import com.v5analytics.webster.annotations.Handle;
@@ -13,30 +12,17 @@ import org.visallo.core.user.User;
 import org.visallo.web.AuthenticationHandler;
 import org.visallo.web.CurrentUser;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
 import java.util.List;
 
 public class Login implements ParameterizedHandler {
 
     private final UserRepository userRepository;
-    private final SSLSocketFactory sslSocketFactory;
 
     @Inject
     public Login(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.sslSocketFactory = createPenteOrgSSLSocketFactory();
     }
 
     @Handle
@@ -73,61 +59,11 @@ public class Login implements ParameterizedHandler {
     }
 
     private boolean isValidPenteOrgUser(String username, String password) {
-        HttpsURLConnection connection = null;
         try {
-            URL url = new URL("https://www.pente.org/gameServer/index.jsp");
-            connection = (HttpsURLConnection) url.openConnection();
-            connection.setSSLSocketFactory(sslSocketFactory);
-            connection.setConnectTimeout(5000);
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            try (OutputStream out = connection.getOutputStream()) {
-                out.write(String.format("name2=%s&password2=%s", username, password).getBytes("UTF-8"));
-            }
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                return false;
-            }
-            List<String> cookieHeaders = connection.getHeaderFields().get("Set-Cookie");
-            connection.disconnect();
+            List<String> cookieHeaders = SiteLogin.login(username, password);
             return cookieHeaders.stream().anyMatch(header -> header.contains(String.format("name2=\"%s\"",username)));
         } catch (IOException e) {
-            throw Throwables.propagate(e);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
-
-    private static SSLSocketFactory createPenteOrgSSLSocketFactory() {
-        // This is necessary because the root CA used by pente.org is not present in the default trust store that
-        // ships with Oracle Java 8.
-        // http://stackoverflow.com/questions/34110426/does-java-support-lets-encrypt-certificates
-        // https://community.letsencrypt.org/t/will-the-cross-root-cover-trust-by-the-default-list-in-the-jdk-jre/134/40
-        try {
-            // Certificates downloaded from https://letsencrypt.org/certs/lets-encrypt-x{1-3}-cross-signed.der
-            final String[] certResources = new String[]{
-                    "lets-encrypt-x1-cross-signed.der",
-                    "lets-encrypt-x2-cross-signed.der",
-                    "lets-encrypt-x3-cross-signed.der"
-            };
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(null, null);
-            int entryNumber = 1;
-            for (String certResource : certResources) {
-                InputStream in = Login.class.getResourceAsStream(certResource);
-                Certificate cert = CertificateFactory.getInstance("X.509").generateCertificate(in);
-                keyStore.setCertificateEntry(Integer.toString(entryNumber++), cert);
-            }
-            TrustManagerFactory trustManagerFactory =
-                    TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(keyStore);
-
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
-            return sslContext.getSocketFactory();
-        } catch (Exception e) {
-            throw Throwables.propagate(e);
+            return false;
         }
     }
 }
